e9fae79239a6fd3ca54bd702d256f0563ea79b72
Kiselev005555@gmail.com <Kiselev005555@gmail.com>
2021-11-10 10:32:59 +0300

    Рабочий клиент

--- Chat/Form1.Designer.cs
+++ Chat/Form1.Designer.cs
@@ -29,11 +29,13 @@ namespace Chat
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // button1
@@ -78,6 +80,10 @@ namespace Chat
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -102,6 +108,7 @@ namespace Chat
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Chat/Form1.cs
+++ Chat/Form1.cs
@@ -20,9 +20,11 @@ namespace Chat
             InitializeComponent();
         }
         NamedPipeClientStream namedPipeClientStream = null;
+        IAsyncResult asyncResult;
+        byte[] bytes = new byte[256];
         private void button1_Click(object sender, EventArgs e)
         {
-            namedPipeClientStream = new NamedPipeClientStream(".", "mypipe");
+            namedPipeClientStream = new NamedPipeClientStream(".", "mypipe",PipeDirection.InOut,PipeOptions.Asynchronous);
             try
             {
                 namedPipeClientStream.Connect(1000);
@@ -30,19 +32,28 @@ namespace Chat
             catch { }
             
             string message = textBox1.Text;
-            
+            asyncResult = namedPipeClientStream.BeginRead(bytes, 0, bytes.Length, null, null);
+            timer1.Enabled = true;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             string message = textBox2.Text;
-            byte[] bytes = Encoding.ASCII.GetBytes(message);
-            namedPipeClientStream.Write(bytes, 0, bytes.Length);
+            byte[] bs = Encoding.ASCII.GetBytes(message);
+            namedPipeClientStream.Write(bs, 0, bs.Length);
             namedPipeClientStream.Flush();
-            namedPipeClientStream.Read(bytes, 0, bytes.Length);
-            string s = Encoding.ASCII.GetString(bytes);
-            listBox1.Items.Add(s);
+        }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (asyncResult.IsCompleted)
+            {
+                Console.WriteLine(bytes.Length);
+                int a=namedPipeClientStream.EndRead(asyncResult);
+                string s = Encoding.ASCII.GetString(bytes,0,a);
+                listBox1.Items.Add(s);
+                asyncResult = namedPipeClientStream.BeginRead(bytes, 0, bytes.Length, null, null);
+            }
         }
     }
 }
